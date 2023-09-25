17d2e96ad6c540340bac57f65e98ff831b58c810
= <feliperos.simao@gmail.com>
2023-09-25 18:42:12 -0300

    update(retryUpdate): implementation of validations in asyncPreLogin

--- src/main/java/br/com/rabbithole/economy/events/OnAsyncPreLoginEvent.java
+++ src/main/java/br/com/rabbithole/economy/events/OnAsyncPreLoginEvent.java
@@ -2,6 +2,7 @@ package br.com.rabbithole.economy.events;
 
 import br.com.rabbithole.economy.Economy;
 import br.com.rabbithole.economy.data.tables.EconomyAccountData;
+import br.com.rabbithole.economy.utils.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,6 +19,7 @@ public class OnAsyncPreLoginEvent implements Listener {
 
     @EventHandler
     public void onAsyncPreLogin(AsyncPlayerPreLoginEvent event) {
+        /*
         EconomyAccountData accountData = new EconomyAccountData(event.getName());
         if (!accountData.hasAccount()) {
             if (!accountData.createAccount()) {
@@ -27,6 +29,27 @@ public class OnAsyncPreLoginEvent implements Listener {
             Economy.getCache().addPlayerInCache(accountData.getPlayerName(), 0.0);
             return;
         }
+        Economy.getCache().addPlayerInCache(accountData.getPlayerName(), accountData.getBalance());
+         */
+        if (Economy.getCache().hasPlayerInRetryCache(event.getName())) {
+            Economy.getCache().addPlayerInCache(event.getName(), Economy.getCache().getPlayerBalanceInRetryCache(event.getName()));
+            Economy.getCache().removePlayerFromRetryCache(event.getName());
+            return;
+        }
+
+        EconomyAccountData accountData = new EconomyAccountData(event.getName());
+        if (!accountData.hasAccount()) {
+            if (!accountData.createAccount()) {
+                Economy.getCommon().getMessages().sendError(
+                        Bukkit.getConsoleSender(),
+                        "Erro ao adicionar Jogador %s ao sistema de Economia!".formatted(event.getName()));
+                event.kickMessage(StringUtils.formatString("<red>Por favor entre novamente!"));
+                event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
+                return;
+            }
+            Economy.getCache().addPlayerInCache(accountData.getPlayerName(), 0.0);
+        }
+
         Economy.getCache().addPlayerInCache(accountData.getPlayerName(), accountData.getBalance());
     }
 }
