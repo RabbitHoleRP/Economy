c2ac2440a373ed04cab632f241f9bd5346358b4c
= <feliperos.simao@gmail.com>
2023-09-26 06:53:41 -0300

    feat(npc): initial implementation of NPCs

--- src/main/java/br/com/rabbithole/economy/events/OnRightClickEvent.java
+++ src/main/java/br/com/rabbithole/economy/events/OnRightClickEvent.java
@@ -1,2 +1,26 @@
-package br.com.rabbithole.economy.events;public class OnRightClickEvent {
+package br.com.rabbithole.economy.events;
+
+import br.com.rabbithole.economy.Economy;
+import br.com.rabbithole.economy.npc.ShopKeeperNPC;
+import net.citizensnpcs.api.event.NPCRightClickEvent;
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
+public class OnRightClickEvent implements Listener {
+    final Plugin plugin;
+
+    public OnRightClickEvent(Plugin plugin) {
+        this.plugin = plugin;
+        this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onNPCRightClick(NPCRightClickEvent event) {
+        NPC npc = event.getNPC();
+        if (npc.getName().equals(ShopKeeperNPC.NAME)) {
+            Economy.getCommon().getMessages().sendSuccess(event.getClicker(), "Olá " + event.getClicker().getName());
+        }
+    }
 }
